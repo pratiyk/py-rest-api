04267e8471df276300f65b84b9520fe0629e00f3
Prateek <pratiyk.shrivastava@gmail.com>
2023-08-08 12:04:11 +0530

    Added line for git testing

--- fetching-api.py
+++ fetching-api.py
@@ -6,3 +6,4 @@ data = response_API.text
 parse_json = json.loads(data)
 active_case = parse_json['Andaman and Nicobar Islands']['districtData']['South Andaman']['active']
 print("Active cases in South Andaman:", active_case)
+print("Testing with github")
